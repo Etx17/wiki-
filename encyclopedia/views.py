4b70a819ed51aa1fb1e92fecdcef30ebd2a9fb87
Etx17 <etiennededianos@gmail.com>
2021-12-16 18:43:47 +0100

    having two first problems done, even if first isn't working as i want, i still go on. Here is the version to take on from by now

---
+++ encyclopedia/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from markdown2 import Markdown
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, entry):
+    markdowner = Markdown()
+    entryPage = util.get_entry(entry)
+    
+    if entryPage is None:
+        return render(request, "encyclopedia/notfound.html", {
+            "entryTitle": entry
+        })
+            
+    else:
+        return render(request, "encyclopedia/entry.html", { 
+            "title": markdowner.convert(entryPage),
+            "entryTitle": entry 
+        })
